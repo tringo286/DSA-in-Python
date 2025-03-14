031e54c297b18086c4ed57ebf14ecc92ab943d4a
Tri Ngo <tringo831@gmail.com>
2025-03-13 21:23:01 -0700

    Create doubly linked list folder

--- 03_Linked_List/01_Singly_Linked_List/insert_at_beginning.py
+++ 03_Linked_List/01_Singly_Linked_List/insert_at_beginning.py
@@ -8,7 +8,7 @@ def insert_at_beginning(ll, data):
 
 
 # Example usage
-if __name__ == "__main__":
+if __name__ == "__main__":  
     ll = SinglyLinkedList()
     insert_at_beginning(ll, 10)
     insert_at_beginning(ll, 20) # Add more operations or display the list as needed
