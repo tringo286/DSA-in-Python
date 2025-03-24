4467a8f46dc86f5787ceb5c5a40c6f9ebde9a2f5
Tri Ngo <tringo831@gmail.com>
2025-03-23 21:58:13 -0700

    Update delete-by-value file in singly linked list folder

--- 03_Linked_List/01_Singly_Linked_List/delete_by_value.py
+++ 03_Linked_List/01_Singly_Linked_List/delete_by_value.py
@@ -1,4 +1,7 @@
 # delete_by_value.py
+
+# 1. Check if the list is empty: If the list is empty (i.e., the head is null), there's nothing to delete.
+# 2. Check if the value to delete is in the head node: If the value is found in the head node, we simply move the head to the next node.
 from linked_list import SinglyLinkedList
 
 def delete_by_value(ll, value):
@@ -8,14 +11,16 @@ def delete_by_value(ll, value):
 
     # If the value to be deleted is the head node
     if ll.head.data == value:
-        ll.head = ll.head.next
+        ll.head = ll.head.next # By setting ll.head = ll.head.next, you are essentially shifting the head of the list to the second node. This makes the second node the new head of the list  
         return
 
     current_node = ll.head
     while current_node.next:
+
+        # This checks if the data in the next node is equal to the value we're looking for
         if current_node.next.data == value:
-            current_node.next = current_node.next.next  # Bypass the node
-            return
+            # Setting current_node.next = current_node.next.next skips the node to be deleted by pointing the current node to the node after it, removing the reference to the deleted node.
+            current_node.next = current_node.next.next  
         current_node = current_node.next
 
     print(f"Value {value} not found in the list.")
@@ -27,9 +32,15 @@ if __name__ == "__main__":
     ll.insert_at_end(30)
     ll.insert_at_end(40)
 
-    print("Before deletion:")
+    print("Before deletion value 20:")
     ll.print_list()
 
     delete_by_value(ll, 20)
     print("After deleting value 20:")
     ll.print_list()
+
+# Before deletion value 20:
+# 10 -> 20 -> 30 -> 40 -> None
+# Value 20 not found in the list.
+# After deleting value 20:
+# 10 -> 30 -> 40 -> None
