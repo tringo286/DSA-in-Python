f2804107441fb8377f603c8bf3874764620497c7
Tri Ngo <tringo831@gmail.com>
2025-04-12 10:00:48 -0700

    Update doubly_linked_list file in doubly linked list folder

--- 03_Linked_List/02_Doubly_Linked_List/doubly_linked_list.py
+++ 03_Linked_List/02_Doubly_Linked_List/doubly_linked_list.py
@@ -19,5 +19,18 @@ class DoublyLinkedList:
         while current_node:
             print(current_node.data, end = ' <-> ')
             current_node = current_node.next
-        print('None')      
-    
+        print('None')  
+
+    def insert_at_end(self, data):
+        new_node = DoublyNode(data)
+
+        if self.head is None:
+            self.head = new_node
+            return 
+
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+
+        current_node.next = new_node
+        new_node.prev = current_node
