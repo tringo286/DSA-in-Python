2f5443d6e86ee272c489ad34c35e9cd6b5ae8e1f
Tri Ngo <tringo831@gmail.com>
2025-04-18 22:39:23 -0700

    Update find_max exercise to arrays folder

--- 02_Arrays/Exercises/05_largest_element.py
+++ 02_Arrays/Exercises/05_largest_element.py
@@ -15,3 +15,29 @@ def find_largest(arr):
 if __name__ == '__main__':
     arr = [10, 324, 45, 90, 9808]
     print(find_largest(arr)) # 9808
+
+# Recursive Approach – O(n) Time and O(n) Space
+
+def findMax(arr, i):
+  
+    # Last index returns the element
+    if i == len(arr) - 1:
+        return arr[i]
+
+    # Find the maximum from the rest of the list
+    recMax = findMax(arr, i + 1)
+
+    # Compare with i-th element and return
+    return max(recMax, arr[i])
+
+def largest(arr):
+    return findMax(arr, 0)
+
+if __name__ == '__main__':
+  arr = [10, 324, 45, 90, 9808, 1]
+  print(largest(arr)) # 9808
+
+def largest(arr):
+    return findMax(arr, 0)
+
+# Using Library Methods – O(n) Time and O(1) Space
