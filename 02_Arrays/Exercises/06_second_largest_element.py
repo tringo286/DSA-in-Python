78ece1b85f41efe6626dfd373033d0a72bff5151
Tri Ngo <tringo831@gmail.com>
2025-04-27 17:31:01 -0700

    Update second-largest-element exercise in  array folder

--- 02_Arrays/Exercises/06_second_largest_element.py
+++ 02_Arrays/Exercises/06_second_largest_element.py
@@ -40,3 +40,33 @@ def getSecondLargest(arr):
 if __name__ == "__main__":
     arr = [12, 35, 1, 10, 34, 1]
     print(getSecondLargest(arr)) # 34 
+
+# 2. [Better Approach] Two Pass Search
+# Python program to find the second largest element in the array
+# using two traversals
+
+# Function to find the second largest element in the array
+def getSecondLargest(arr):
+    n = len(arr)
+
+    largest = -1
+    secondLargest = -1
+
+    # Finding the largest element
+    for i in range(n):
+        if arr[i] > largest:
+            largest = arr[i]
+
+    # Finding the second largest element
+    for i in range(n):
+        
+        # Update second largest if the current element is greater
+        # than second largest and not equal to the largest
+        if arr[i] > secondLargest and arr[i] != largest:
+            secondLargest = arr[i]
+    
+    return secondLargest
+
+if __name__ == "__main__":
+    arr = [12, 35, 1, 10, 34, 1]
+    print(getSecondLargest(arr)) # 34 
