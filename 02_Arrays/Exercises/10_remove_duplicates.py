65bf2f10abef21ee7380e1252a132bef9afe8514
Tri Ngo <tringo831@gmail.com>
2025-05-04 22:55:29 -0700

    Update remove duplicates exercise in arrays folders

--- 02_Arrays/Exercises/10_remove_duplicates.py
+++ 02_Arrays/Exercises/10_remove_duplicates.py
@@ -42,4 +42,28 @@ if __name__ == "__main__":
     newSize = removeDuplicates(arr)
 
     for i in range(newSize):
-        print(arr[i], end=" ")
+        print(arr[i], end=" ") # 1 2 3 4 5
+
+# 2. Expected Approach – O(n) Time and O(1) Space
+
+def removeDuplicates(arr):
+    n = len(arr)
+    if n <= 1:
+        return n
+
+    # Start from the second element
+    idx = 1  
+    
+    for i in range(1, n):
+        if arr[i] != arr[i - 1]:
+            arr[idx] = arr[i]
+            idx += 1
+
+    return idx
+
+if __name__ == "__main__":
+    arr = [1, 2, 2, 3, 4, 4, 4, 5, 5]
+    newSize = removeDuplicates(arr)
+
+    for i in range(newSize):
+        print(arr[i], end=" ") # 1 2 3 4 5
