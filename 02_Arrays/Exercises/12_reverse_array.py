c491d172c5682a13d93d7b73d19edb9492c59426
Tri Ngo <tringo831@gmail.com>
2025-05-14 07:50:33 -0700

    Update reverse array exercise in arrays folder

--- 02_Arrays/Exercises/12_reverse_array.py
+++ 02_Arrays/Exercises/12_reverse_array.py
@@ -37,3 +37,39 @@ if __name__ == "__main__":
   
     for i in range(len(arr)):
         print(arr[i], end=" ") # 5 6 2 3 4 1 
+
+# 2. [Expected Approach – 1] Using Two Pointers – O(n) Time and O(1) Space
+
+# The idea is to maintain two pointers: left and right, such that left points at the beginning of the array and right points to the end of the array. 
+
+
+# While left pointer is less than the right pointer, swap the elements at these two positions. After each swap, increment the left pointer and decrement the right pointer to move towards the center of array. This will swap all the elements in the first half with their corresponding element in the second half.
+
+# Python Program to reverse an array using Two Pointers
+
+# function to reverse an array
+def reverseArray(arr):
+    
+    # Initialize left to the beginning and right to the end
+    left = 0
+    right = len(arr) - 1
+  
+    # Iterate till left is less than right
+    while left < right:
+        
+        # Swap the elements at left and right position
+        arr[left], arr[right] = arr[right], arr[left]
+      
+        # Increment the left pointer
+        left += 1
+      
+        # Decrement the right pointer
+        right -= 1
+
+if __name__ == "__main__":
+    arr = [1, 4, 3, 2, 6, 5]
+
+    reverseArray(arr)
+  
+    for i in range(len(arr)):
+        print(arr[i], end=" ") # 5 6 2 3 4 1 
