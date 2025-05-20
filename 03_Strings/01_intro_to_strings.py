10a6588f6401813e8b081bc9e7d607054b699c7b
Tri Ngo <tringo831@gmail.com>
2025-05-20 10:16:47 -0700

    Create strings folder

---
+++ 03_Strings/01_intro_to_strings.py
@@ -0,0 +1,38 @@
+# Introduction to Strings
+
+# Strings are sequences of characters. The differences between a character array and a string are, a string is terminated with a special character ‘\0’ and strings are typically immutable in most of the programming languages like Java, Python and JavaScript. Below are some examples of strings:
+
+# "geeks" , "for", "geeks", "GeeksforGeeks", "Geeks for Geeks", "123Geeks", "@123 Geeks"
+
+# How to Declare Strings in Python
+
+# Python: Strings are dynamic and can be declared using single, double, or triple quotes, making them flexible for multi-line text handling.
+
+# Python Program for
+# Creation of String
+
+# Creating a String
+# with single Quotes
+String1 = 'Welcome to the Geeks World'
+print("String with the use of Single Quotes: ")
+print(String1)
+
+# Creating a String
+# with double Quotes
+String1 = "I'm a Geek"
+print("\nString with the use of Double Quotes: ")
+print(String1)
+
+# Creating a String
+# with triple Quotes
+String1 = '''I'm a Geek and I live in a world of "Geeks"'''
+print("\nString with the use of Triple Quotes: ")
+print(String1)
+
+# Creating String with triple
+# Quotes allows multiple lines
+String1 = '''Geeks
+            For
+            Life'''
+print("\nCreating a multiline String: ")
+print(String1)
---
+++ 03_Strings/README.md
@@ -0,0 +1,11 @@
+# String in Data Structure
+
+A string is a sequence of characters. The following facts make string an interesting data structure.
+
+- Small set of elements. Unlike normal array, strings typically have smaller set of items. For example, lowercase English alphabet has only 26 characters. ASCII has only 256 characters.
+- Strings are immutable in programming languages like Java, Python, JavaScript and C#.
+- Many String Problems can optimized using the fact that the character set size is small. For example sorting can be done faster, counting frequencies of items is faster and many interesting interview questions are based on this.
+
+## 1. Basics of Strings:
+
+**intro_to_strings.py**
