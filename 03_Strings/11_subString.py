138241dce445585523af4420975a1a78e46ced11
Tri Ngo <tringo831@gmail.com>
2025-06-23 21:57:34 -0700

    Update subString file

--- 03_Strings/11_subString.py
+++ 03_Strings/11_subString.py
@@ -33,3 +33,54 @@ s = 'abc'
 res = find_substrings(s)
 for i in res:
     print(i, end=' ') # a ab abc b bc c 
+
+# 2. [Interesting Approach] - Using Recursion
+
+# The idea is to recursively generate all possible substrings of the given string s. To do so, create an array of string res[] to store the substrings of string s and an empty string cur to store the current string. Start from the 0th index and for each index ind, add the current character s[ind] in the string cur, and add the string cur in res[]. Then, move to index ind + 1, and recursively generate the all substrings. At each recursive call, check if string cur is empty, if so, skip the current character to start the string from next index ind + 1. At last print all the substrings.
+
+# Python program to find all the
+# substrings of given string
+
+# Recursive Function to find all
+# substrings of a string
+def subString(s, n, index, cur, res):
+
+    # if we have reached the
+    # end of the string
+    if index == n:
+        return
+
+    # add the character s[index]
+    # to the current string
+    cur += s[index]
+
+    # add the current string in result
+    res.append(cur)
+
+    # move to next index
+    subString(s, n, index + 1, cur, res)
+
+    # remove the current character
+    # from the current string
+    cur = cur[:-1]
+
+    # if current string is empty
+    # skip the current index to
+    # start the new substring
+    if not cur:
+        subString(s, n, index + 1, cur, res)
+
+# Function to find all substrings
+def findSubstrings(s):
+
+    # to store all substrings
+    res = []
+
+    # to store current string
+    cur = ""
+    subString(s, len(s), 0, cur, res)
+    return res
+
+s = "abc"
+res = findSubstrings(s)
+print(" ".join(res)) # a ab abc b bc c 
