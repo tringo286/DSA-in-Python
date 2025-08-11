aaaf0464dbe8de1038ccc1423a3ec476c680fc62
Tri Ngo <tringo831@gmail.com>
2025-08-11 16:16:01 -0700

    Update modifyString file

--- 03_Strings/Easy Problems/14_modifyString.py
+++ 03_Strings/Easy Problems/14_modifyString.py
@@ -90,3 +90,41 @@ if __name__ == "__main__" :
     modifyString(str)
 
 # g2 e4 k2 s2 f1 o1 r1 
+
+# 3. [Expected Approach 2] Use Frequency Array - O(n) Time and O(MAX_CHAR) Space
+
+# Create a frequency array to store frequency of each character. We are going to use characters as index in this array. The frequency of 'a' is going to be stored at index 0, 'b' at 1, and so on. To find the index of a character, we subtract character a's ASCII value from the ASCII value of the character.
+
+# Traverse the string again and check whether the frequency of that character is 0 or not. If not 0, then print the character along with its frequency and update its frequency to 0 in the frequency array. This is done so that the same character is not printed again. This is the same approach as above, but instead of using library hash map, we use an array to ensure that we get the fast results.
+
+# C++ Code to find character frequencies in order of
+# occurrence using Frequency Array
+MAX_CHAR = 26
+
+# function to modify the string by appending
+# character with its frequency in order of occurrence
+def modifyString(s):
+  
+    # Store frequencies of all characters
+    freq = [0] * MAX_CHAR
+    for c in s:
+        freq[ord(c) - ord('a')] += 1
+    
+    res = ""
+    for c in s:
+        if freq[ord(c) - ord('a')] != 0:
+          
+            # append character and frequency
+            res += c + str(freq[ord(c) - ord('a')]) + " "
+            
+            # mark as processed
+            freq[ord(c) - ord('a')] = 0
+    
+    return res
+
+s = "geeksforgeeks"
+print(modifyString(s)) # g2e4k2s2f1o1r1
+
+
+
+
